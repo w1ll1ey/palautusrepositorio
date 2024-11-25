fc369790a6c582c8afc0019831f629be43dbfe87
w1ll1ey <vili.ilari.kaisto@gmail.com>
2024-11-25 22:44:53 +0200

    viikko4

---
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -0,0 +1,15 @@
+HINTA = 5
+
+
+class Kassapaate:
+    def __init__(self):
+        self.__myytyja_lounaita = 0
+
+    def lataa(self, kortti, summa):
+        if summa > 0:
+            kortti.lataa(summa)
+
+    def osta_lounas(self, kortti):
+        if kortti.saldo() >= HINTA:
+            kortti.osta(HINTA)
+        self.__myytyja_lounaita = self.__myytyja_lounaita + 1
